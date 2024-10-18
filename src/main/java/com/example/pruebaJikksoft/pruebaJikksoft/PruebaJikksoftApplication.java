531c8a783e3e64a98e5a465cef10fd23ada39b98
Andres Perez <perezandres1799@gmail.com>
2024-10-17 19:37:32 -0500

    Proyecto base

---
+++ src/main/java/com/example/pruebaJikksoft/pruebaJikksoft/PruebaJikksoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebaJikksoft.pruebaJikksoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaJikksoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaJikksoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pruebaJikksoft/pruebaJikksoft/PruebaJikksoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebaJikksoft.pruebaJikksoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaJikksoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
